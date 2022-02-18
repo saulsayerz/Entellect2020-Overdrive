3c93fd57ad9589c255b11f9eb410d2d42268840c
raniadf <70957597+raniadf@users.noreply.github.com>
2022-02-18 18:52:15 +0700

    Delete starter-pack/starter-bots/python3 directory

--- starter-pack/starter-bots/python3/Lane.py
+++
@@ -1,5 +0,0 @@
-class Lane:
-    def __init__(self, position, objectType, player_id):
-        self.position = position
-        self.object = objectType
-        self.occupied_by_player_id = player_id
--- starter-pack/starter-bots/python3/Position.py
+++
@@ -1,4 +0,0 @@
-class Position:
-    def __init__(self, y, x):
-        self.lane = y
-        self.block = x
